ace76db342a3db47c0ad9d5ae5ccaa1ad7c6a797
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-06-03 23:52:42 +0000

    pom

---
+++ archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-representation-impl/src/main/java/__systemIdentifier__/representation/impl/Application.java
@@ -0,0 +1,9 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${systemIdentifier}.representation.impl;
+
+@javax.ws.rs.ApplicationPath("/")
+public class Application extends javax.ws.rs.core.Application {
+
+}
