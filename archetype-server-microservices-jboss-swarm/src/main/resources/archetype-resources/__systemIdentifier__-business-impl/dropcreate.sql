c76655d9536bd253fa6039351493ee16c2e6076e
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-06-03 18:47:52 +0000

    pom

--- archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-business-impl/dropcreate.sql
+++
@@ -1,2 +0,0 @@
-drop schema cyk_${systemIdentifier}_db;
-create database cyk_${systemIdentifier}_db;
--- archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-representation-entities/src/main/java/ui/web/primefaces/Action.java
+++
@@ -1,9 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.ui.web.primefaces;
-
-public enum Action{
-	
-	;
-}
