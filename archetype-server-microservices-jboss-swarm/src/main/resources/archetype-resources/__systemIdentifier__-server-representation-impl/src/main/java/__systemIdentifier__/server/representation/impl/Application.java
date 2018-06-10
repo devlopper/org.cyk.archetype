873732712fa6a37386c604a06c1d5c48bd5fefa7
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-06-10 17:12:59 +0000

    jee system client archetype

---
+++ archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-server-representation-impl/src/main/java/__systemIdentifier__/server/representation/impl/Application.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${systemIdentifier}.server.representation.impl;
+
+@javax.ws.rs.ApplicationPath("/")
+@javax.ws.rs.Path("/")
+javax.enterprise.context.ApplicationScoped
+public class Application extends javax.ws.rs.core.Application {
+
+	@javax.ws.rs.GET
+	@javax.ws.rs.Path("/")
+	@javax.ws.rs.Produces(MediaType.TEXT_PLAIN)
+	public String getRootResource() {
+		return "Hi from ${systemIdentifier} system server. Time is "+new Date();
+	}
+	
+}
