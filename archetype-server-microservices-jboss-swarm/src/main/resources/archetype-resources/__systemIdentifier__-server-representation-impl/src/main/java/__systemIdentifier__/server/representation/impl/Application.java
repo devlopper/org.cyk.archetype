478a8764552b07ba4398a30ac18c9691991ba88a
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2018-06-10 18:38:52 +0000

    client and server

--- archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-server-representation-impl/src/main/java/__systemIdentifier__/server/representation/impl/Application.java
+++ archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-server-representation-impl/src/main/java/__systemIdentifier__/server/representation/impl/Application.java
@@ -5,14 +5,14 @@ package ${package}.${systemIdentifier}.server.representation.impl;
 
 @javax.ws.rs.ApplicationPath("/")
 @javax.ws.rs.Path("/")
-javax.enterprise.context.ApplicationScoped
+@javax.enterprise.context.ApplicationScoped
 public class Application extends javax.ws.rs.core.Application {
 
 	@javax.ws.rs.GET
 	@javax.ws.rs.Path("/")
-	@javax.ws.rs.Produces(MediaType.TEXT_PLAIN)
+	@javax.ws.rs.Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
 	public String getRootResource() {
-		return "Hi from ${systemIdentifier} system server. Time is "+new Date();
+		return "Hi from ${systemIdentifier} system server. Time is "+new java.util.Date();
 	}
 	
 }
