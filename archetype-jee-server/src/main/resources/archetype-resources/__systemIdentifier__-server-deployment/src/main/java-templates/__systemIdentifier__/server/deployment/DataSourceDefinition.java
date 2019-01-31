694ad5229dc0c250ab90b1378a5c5ae50c8d132b
Christian Yao Komenan <kycdev@gmail.com>
2019-01-31 11:13:27 +0000

    commit

---
+++ archetype-jee-server/src/main/resources/archetype-resources/__systemIdentifier__-server-deployment/src/main/java-templates/__systemIdentifier__/server/deployment/DataSourceDefinition.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${systemIdentifier}.server.deployment;
+
+@javax.annotation.sql.DataSourceDefinition(
+		name="${data.source.jndi.name}",
+		className="${data.source.className}",
+		url="${data.source.url}",
+		user="${data.source.user}",
+		password="${data.source.password}"
+)
+public class DataSourceDefinition {}
