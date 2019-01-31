b9af06beb0113ca4ea66b7bd07ce25297eb093f3
Christian Yao Komenan <kycdev@gmail.com>
2019-01-31 09:46:49 +0000

    commit

---
+++ archetype-server-microservices-jboss-swarm/src/main/resources/archetype-resources/__systemIdentifier__-server-business-impl/src/test/java/__systemIdentifier__/server/business/impl/integration/DataSourceDefinition.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${systemIdentifier}.server.business.impl.integration;
+
+@javax.annotation.sql.DataSourceDefinition(
+		name="java:/${systemIdentifier}/server/dataSource",
+		className="org.h2.jdbcx.JdbcDataSource",
+		url="jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		user="sa",
+		password="sa"
+)
+public class DataSourceDefinition {}
